1e04139c5c949219df9dd39edca122e90b7d5ec2
Ricardogn224 <abrahamricardogn224@gmail.com>
2024-04-09 16:05:21 +0200

    init of database add

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module flashEvent
 go 1.22.1
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/janirefdez/ArticleRestApi v0.0.0-20221015153905-75a92e5d1e2e // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+	github.com/janirefdez/ArticleRestApi v0.0.0-20221015153905-75a92e5d1e2e
+	github.com/lib/pq v1.10.9
 )
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/janirefdez/ArticleRestApi/pkg/mocks"
 	_ "github.com/lib/pq"
 )
 
@@ -25,14 +26,47 @@ func Connect() *sql.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Successfully connected to db!")
+
+	log.Println("Successfully connected to db!")
+
 	return db
 }
 
 func CloseConnection(db *sql.DB) {
 	defer db.Close()
+}
+
+func CreateTable(db *sql.DB) {
+	var exists bool
+	if err := db.QueryRow("SELECT EXISTS (SELECT FROM pg_tables WHERE  schemaname = 'public' AND tablename = 'articles' );").Scan(&exists); err != nil {
+		log.Println("failed to execute query", err)
+		return
+	}
+	if !exists {
+		results, err := db.Query("CREATE TABLE articles (id VARCHAR(36) PRIMARY KEY, title VARCHAR(100) NOT NULL, description VARCHAR(50) NOT NULL, content VARCHAR(50) NOT NULL);")
+		if err != nil {
+			log.Println("failed to execute query", err)
+			return
+		}
+		log.Println("Table created successfully", results)
+
+		for _, article := range mocks.Articles {
+			queryStmt := `INSERT INTO articles (id,title,description,content) VALUES ($1, $2, $3, $4) RETURNING id;`
+
+			err := db.QueryRow(queryStmt, &article.Id, &article.Title, &article.Desc, &article.Content).Scan(&article.Id)
+			if err != nil {
+				log.Println("failed to execute query", err)
+				return
+			}
+		}
+		log.Println("Mock Articles included in Table", results)
+	} else {
+		log.Println("Table 'articles' already exists ")
+	}
+
 }
