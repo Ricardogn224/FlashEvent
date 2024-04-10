7e810a8b59b08e3b131394e25a62637516fe33b0
Ricardogn224 <abrahamricardogn224@gmail.com>
2024-04-10 13:01:26 +0200

    add of swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,52 @@
 package main
 
 import (
-	"database/sql"
-
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq" // Import du pilote PostgreSQL
 
 	"flashEvent/pkg/db"
 	"flashEvent/pkg/handlers"
+
+	"github.com/swaggo/http-swagger"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Article REST API!")
-	fmt.Println("Article REST API")
-}
+// @title Article REST API
+// @version 1.0
+// @description API de gestion des articles
+// @host localhost:8080
+// @BasePath /v1
+func main() {
+	DB := db.Connect()
+	db.CreateTable(DB)
+	defer db.CloseConnection(DB)
 
-func handleRequests(DB *sql.DB) {
 	h := handlers.New(DB)
-	// create a new instance of a mux router
+
+	// Création d'un routeur mux
 	myRouter := mux.NewRouter().StrictSlash(true)
+
+	// Définition des endpoints de l'API avec les handlers correspondants
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", h.GetAllArticles).Methods(http.MethodGet)
 	myRouter.HandleFunc("/articles/{id}", h.GetArticle).Methods(http.MethodGet)
 	myRouter.HandleFunc("/articles", h.AddArticle).Methods(http.MethodPost)
 	myRouter.HandleFunc("/articles/{id}", h.UpdateArticle).Methods(http.MethodPut)
 	myRouter.HandleFunc("/articles/{id}", h.DeleteArticle).Methods(http.MethodDelete)
+
+	// Route pour servir la documentation Swagger
+	myRouter.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("/swagger/doc.json"), // Spécifiez le chemin où se trouve le fichier JSON de Swagger
+	))
+
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
-	fmt.Println("Listening in port 8080")
+	fmt.Println("Écoute sur le port 8080")
 }
 
-func main() {
-	DB := db.Connect()
-	db.CreateTable(DB)
-	handleRequests(DB)
-	db.CloseConnection(DB)
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bienvenue sur l'API REST d'Article!")
+	fmt.Println("API REST d'Article")
 }
