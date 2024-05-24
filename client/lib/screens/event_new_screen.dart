7d672b0b8a8f6ecb5f38f29a22dff31543766752
Jerrinald <jerrinald95190@live.fr>
2024-05-24 18:50:16 +0200

    api connection client

--- client/lib/screens/event_new_screen.dart
+++ client/lib/screens/event_new_screen.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flash_event/core/services/event_services.dart';
+import 'package:flutter_flash_event/core/models/event.dart';
+import 'events_screen.dart';
 
-class EventNewScreen extends StatelessWidget {
+class EventNewScreen extends StatefulWidget {
   const EventNewScreen({Key? key}) : super(key: key);
+
+  @override
+  _EventNewScreenState createState() => _EventNewScreenState();
+}
+
+class _EventNewScreenState extends State<EventNewScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  void _submitEvent() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      Event newEvent = Event(
+        title: _titleController.text,
+        description: _descriptionController.text,
+      );
+
+      try {
+        final response = await EventServices.addEvent(newEvent);
+        if (response.statusCode == 201) {
+          // Event creation successful, navigate to events screen
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => EventsScreen()),
+          );
+        } else {
+          // Handle error
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Event creation failed')),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Padding(
-        padding: const EdgeInsets.only(top: 60.0), // Adjust top padding for the entire column
+        padding: const EdgeInsets.only(top: 60.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.start, // Center the content horizontally
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 ElevatedButton(
                   onPressed: () {
@@ -22,7 +78,7 @@ class EventNewScreen extends StatelessWidget {
                   },
                   child: Text('Back'),
                 ),
-                SizedBox(width: 38), // Add spacing between the "Back" button and the "New Event" title
+                SizedBox(width: 38),
                 Text(
                   'New Event',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
@@ -30,39 +86,55 @@ class EventNewScreen extends StatelessWidget {
               ],
             ),
             SizedBox(height: 40),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Center the content vertically
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Title',
-                      border: OutlineInputBorder(),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        hintText: 'Title',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a title';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Description',
-                      border: OutlineInputBorder(),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextFormField(
+                      controller: _descriptionController,
+                      decoration: InputDecoration(
+                        hintText: 'Description',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a description';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Add your form submission logic here
-                  },
-                  child: Text('Submit'),
-                ),
-              ],
+                  _isLoading
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                    onPressed: _submitEvent,
+                    child: Text('Submit'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-
     );
   }
 }
