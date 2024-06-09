a48116f066b3a2dbef419ee44b772bc75ec1ed52
Jerrinald <jerrinald95190@live.fr>
2024-06-09 17:08:47 +0200

    form event bloc 1

--- client/lib/formEventParty/form_event_party_screen.dart
+++ client/lib/formEventParty/form_event_party_screen.dart
@@ -11,77 +11,82 @@ class BlocFormEventScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form Validation'),
-      ),
-      body: BlocBuilder<FormEventPartyBloc, FormEventPartyState>(builder: (context, state) {
-        return Container(
-          padding: const EdgeInsets.all(30),
-          child: Form(
-            key: state.formKey,
-            child: Column(
-              children: [
-                CustomFormField(
-                  hintText: 'Name',
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                      RegExp(r"[a-zA-Z]+|\s"),
+    return BlocProvider(
+      create: (context) => FormEventPartyBloc()..add(InitEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Form Validation'),
+        ),
+        body: BlocBuilder<FormEventPartyBloc, FormEventPartyState>(
+          builder: (context, state) {
+            return Container(
+              padding: const EdgeInsets.all(30),
+              child: Form(
+                key: state.formKey,
+                child: Column(
+                  children: [
+                    CustomFormField(
+                      hintText: 'Name',
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(
+                          RegExp(r"[a-zA-Z]+|\s"),
+                        )
+                      ],
+                      onChange: (val) {
+                        BlocProvider.of<FormEventPartyBloc>(context)
+                            .add(NameChanged(name: BlocFormItem(value: val!)));
+                      },
+                      validator: (val) {
+                        return state.name.error;
+                      },
+                    ),
+                    CustomFormField(
+                      hintText: 'Description',
+                      onChange: (val) {
+                        BlocProvider.of<FormEventPartyBloc>(context)
+                            .add(DescriptionChanged(description: BlocFormItem(value: val!)));
+                      },
+                      validator: (val) {
+                        return state.description.error;
+                      },
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            BlocProvider.of<FormEventPartyBloc>(context).add(FormSubmitEvent(
+                              onSuccess: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                                );
+                              },
+                              onError: (errorMessage) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text(errorMessage)),
+                                );
+                              },
+                            ));
+                          },
+                          child: const Text('SUBMIT'),
+                        ),
+                        const SizedBox(width: 20),
+                        ElevatedButton(
+                          onPressed: () {
+                            BlocProvider.of<FormEventPartyBloc>(context).add(const FormResetEvent());
+                          },
+                          child: const Text('RESET'),
+                        ),
+                      ],
                     )
                   ],
-                  onChange: (val) {
-                    BlocProvider.of<FormEventPartyBloc>(context)
-                        .add(NameChanged(name: BlocFormItem(value: val!)));
-                  },
-                  validator: (val) {
-                    return state.name.error;
-                  },
                 ),
-                CustomFormField(
-                  hintText: 'Description',
-                  onChange: (val) {
-                    BlocProvider.of<FormEventPartyBloc>(context)
-                        .add(DescriptionChanged(description: BlocFormItem(value: val!)));
-                  },
-                  validator: (val) {
-                    return state.description.error;
-                  },
-                ),
-                const SizedBox(height: 30),
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        BlocProvider.of<FormEventPartyBloc>(context).add(FormSubmitEvent(
-                          onSuccess: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => HomeScreen()),
-                            );
-                          },
-                          onError: (errorMessage) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(content: Text(errorMessage)),
-                            );
-                          },
-                        ));
-                      },
-                      child: const Text('SUBMIT'),
-                    ),
-                    const SizedBox(width: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        BlocProvider.of<FormEventPartyBloc>(context).add(const FormResetEvent());
-                      },
-                      child: const Text('RESET'),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        );
-      }),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
