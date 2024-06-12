7a24ea367c8b76d9589955f947eebfd1096af991
Jerrinald <jerrinald95190@live.fr>
2024-06-12 20:10:53 +0200

    particpant

---
+++ client/lib/participant/bloc/participant_state.dart
@@ -0,0 +1,28 @@
+part of 'participant_bloc.dart';
+
+
+enum EventPartyStatus { initial, loading, success, error }
+
+class EventPartyState {
+  final EventPartyStatus status;
+  final Event? eventParty;
+  final String? errorMessage;
+
+  EventPartyState({
+    this.status = EventPartyStatus.initial,
+    this.eventParty,
+    this.errorMessage,
+  });
+
+  EventPartyState copyWith({
+    EventPartyStatus? status,
+    Event? eventParty,
+    String? errorMessage,
+  }) {
+    return EventPartyState(
+      status: status ?? this.status,
+      eventParty: eventParty ?? this.eventParty,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
