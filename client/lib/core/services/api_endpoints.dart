b6716f32bf14f9e03eb17933e2ffe3ae873943f9
Jerrinald <jerrinald95190@live.fr>
2024-07-18 22:00:18 +0200

    https url1

--- client/lib/core/services/api_endpoints.dart
+++ client/lib/core/services/api_endpoints.dart
@@ -2,5 +2,5 @@ class ApiEndpoints {
   static const String prodUrl = 'eventpresta.fr';
   static const String devUrl = '10.0.2.2:8000';
 
-  static const String baseUrl = devUrl;
+  static const String baseUrl = prodUrl;
 }
